2b1e383ec002b7984537694eab0f2fd6719faadd
VICTOR <Victor.cotrim@live.com>
2025-10-13 19:56:38 -0300

    Atualizando projeto com áudio player e layout responsivo

--- the-hydrogen.github.io/script.js
+++ the-hydrogen.github.io/script.js
@@ -1,18 +1,36 @@
-$(document).ready(function() {
-    $("footer").load("files/footer.html");
-    $("nav").load("files/nav.html");
+$(document).ready(function () {
+    // Detectar basePath automaticamente para arquivos incluídos dinamicamente, ex: nav e footer
+    function getBasePath() {
+        var path = window.location.pathname;
+        var parts = path.split('/').filter(function(part) { return part.length > 0; });
+        if(parts.length > 1) {
+            return "../"; // se estiver em subpasta, sobe um nível
+        } else {
+            return ""; // raiz da pasta
+        }
+    }
 
-    // ADICIONAR ÍCONE DE PLAY 
-    $("audio").not("#musica").before("<i class=\"far fa-play-circle\"></i>");
-    $(".gif").append("<i class=\"fas fa-play\"></i>");
-    // ADICIONAR BOTÃO DE DOWNLOAD
-    $('.back').not('.backgold').append("<a><button style='margin: 0 auto; margin-top: 5px; width: 143px' class=\"baixar btn btn-block btn-default\"><i class='fas fa-arrow-alt-circle-down'></i> Baixar</button></a>");
+    var basePath = getBasePath();
 
-    $("audio").on('ended', function() {
+    // Carregar nav e footer dinamicamente, usando basePath
+    $("nav").load(basePath + "files/nav.html");
+    $("footer").load(basePath + "files/footer.html");
+
+    // Adicionar ícone de play antes de cada audio (exceto '#musica')
+    $("audio").not("#musica").before('<i class="far fa-play-circle"></i>');
+    $(".gif").append('<i class="fas fa-play"></i>');
+
+    // Adicionar botão de download aos painéis (.back exceto .backgold)
+    $('.back').not('.backgold').append('<a><button style="margin: 0 auto; margin-top: 5px; width: 143px" class="baixar btn btn-block btn-default"><i class="fas fa-arrow-alt-circle-down"></i> Baixar</button></a>');
+
+    // Controlar troca de ícones no fim do áudio
+    $("audio").on('ended', function () {
         $(this).siblings("i").toggleClass("fa-pause-circle fa-play-circle");
     });
 
-    $('.ativarTodos').click(function() {
+    // Controlar ação do botão 'ativarTodos'
+    $('.ativarTodos').click(function () {
+        var url = location.pathname.substring(location.pathname.lastIndexOf('/') + 1).split('.')[0];
         if (url == 'gifs') {
             $('.gif > img').click();
         } else {
@@ -20,28 +38,20 @@ $(document).ready(function() {
         }
     });
 
-    // BOTÃO DOWNLOAD
-
+    // Preparar links para download a partir do src do áudio
     var botoes = $(".back");
-
-    //Adicionar o link para download a partir do endereço do áudio
-    for (var i = 0; i < botoes.length; i++) {
-        var b = $(botoes[i]);
+    botoes.each(function () {
+        var b = $(this);
         b.children("a").attr("href", b.children(".botao").children("audio").attr('src'));
         b.children("a").attr("download", '');
-    }
+    });
 
-    // -------
-
-    $(".botao").click(function(e) {
-        //Se tiver a classe 'baixar' significa que o botão de Download foi clicado e não o botão Play
-        // if ($(e.target).hasClass("baixar")) return; //retorna para cancelar
-
+    // Configurar play/pause no botão do player
+    $(".botao").click(function (e) {
         var play = $(this).children("audio")[0];
-
         var toggle = $(this).children("i")[0];
         $(toggle).toggleClass("fa-pause-circle fa-play-circle");
-        if (play.paused == true) {
+        if (play.paused) {
             play.play();
         } else {
             play.pause();
@@ -49,36 +59,30 @@ $(document).ready(function() {
         }
     });
 
-    // GIFS E IMAGEM DOWNLOAD
-    //adiciona botão de download
-    $('.backgif ,.backimg').append("<a><button style='margin: 0 auto; margin-top: 5px;' class=\"baixar btn btn-block btn-default\"><i class='fas fa-arrow-alt-circle-down'></i> Baixar</button></a>");
-    $('.backgif').children('a').children('button').css('width', '300px');
-    $('.backimg').children('a').children('button').css('width', '250px');
-    var gifs = $(".backgif");
+    // Download para gifs e imagens
+    $('.backgif, .backimg').append('<a><button style="margin: 0 auto; margin-top: 5px;" class="baixar btn btn-block btn-default"><i class="fas fa-arrow-alt-circle-down"></i> Baixar</button></a>');
+    $('.backgif a button').css('width', '300px');
+    $('.backimg a button').css('width', '250px');
 
-    for (var i = 0; i < gifs.length; i++) {
-        var b = $(gifs[i]);
+    $('.backgif').each(function () {
+        var b = $(this);
         b.children("a").attr("href", b.children(".gif").children('img').attr('src').replace('/sprite', '').replace('.png', '.gif'));
         b.children("a").attr("download", '');
-    }
+    });
 
-    var gifs = $(".backimg");
-
-    for (var i = 0; i < gifs.length; i++) {
-        var b = $(gifs[i]);
-        b.children("a").attr("href", b.children('.img').children("img").attr('src'));
+    $('.backimg').each(function () {
+        var b = $(this);
+        b.children("a").attr("href", b.children(".img").children("img").attr('src'));
         b.children("a").attr("download", '');
-    }
+    });
 
-    // 
-    setTimeout(function() {
+    // Twitch API checagem de status de stream
+    setTimeout(function () {
         $.ajax({
-            url: "https://api.twitch.tv/kraken/streams/" + 'thehydrogen',
+            url: "https://api.twitch.tv/kraken/streams/thehydrogen",
             dataType: 'json',
-            headers: {
-                'Client-ID': '8as214cog5fxuqjdawgty1cn68ywjv'
-            },
-            success: function(channel) {
+            headers: {'Client-ID': '8as214cog5fxuqjdawgty1cn68ywjv'},
+            success: function (channel) {
                 if (channel["stream"] == null) {
                     $('#stream').html("<b>Stream <b style='color: red'>OFFLINE</span></b>");
                 } else {
@@ -88,56 +92,39 @@ $(document).ready(function() {
         });
     }, 1500);
 
-    $('.exit').click(function() {
-        if ($(this).parent().hasClass('comentar')) { //Se for 'comentar' significa que este é o Exit do caixa de comentários
-            $(this).parent().toggle('slide', {
-                direction: 'right'
-            }, 400);
-            $('.naoCarregando').toggle('slide', {
-                direction: 'right'
-            }, 400);
-            $('.toggleComentar').toggle('slide', {
-                direction: 'right'
-            }, 400);
-        } else if ($(this).hasClass('toggleBaloes')) { //Se tiver 'toggleBaloes' significa que é o Exit dos balões de informações
-            $('.baloes').css({
-                'width': 410,
-                'height': 510
-            });
-            $('.baloes').toggle('slide', {
-                direction: 'right'
-            }, 330);
+    // Eventos para fechar comentários e balões
+    $('.exit').click(function () {
+        if ($(this).parent().hasClass('comentar')) {
+            $(this).parent().toggle('slide', { direction: 'right' }, 400);
+            $('.naoCarregando').toggle('slide', { direction: 'right' }, 400);
+            $('.toggleComentar').toggle('slide', { direction: 'right' }, 400);
+        } else if ($(this).hasClass('toggleBaloes')) {
+            $('.baloes').css({ width: 410, height: 510 });
+            $('.baloes').toggle('slide', { direction: 'right' }, 330);
             if ($(this).css('right') == '225px') {
                 $(this).css('right', 5);
             } else {
                 $(this).css('right', 225);
             }
             $(this).toggleClass('fa-arrow-right fa-arrow-left');
-            setTimeout(function() {
-                $('.baloes').css({
-                    'width': 0,
-                    'height': 0
-                });
+            setTimeout(function () {
+                $('.baloes').css({ width: 0, height: 0 });
             }, 401);
         } else {
             $(this).parent().toggle('fade', 400);
         }
     });
 
-    $('.toggleComentar').click(function() {
-        $(this).toggle('slide', {
-            direction: 'right'
-        }, 400);
-        $('.naoCarregando').toggle('slide', {
-            direction: 'right'
-        }, 400);
-        $('.comentar').toggle('slide', {
-            direction: 'right'
-        }, 400);
+    // Toggle para comentário
+    $('.toggleComentar').click(function () {
+        $(this).toggle('slide', { direction: 'right' }, 400);
+        $('.naoCarregando').toggle('slide', { direction: 'right' }, 400);
+        $('.comentar').toggle('slide', { direction: 'right' }, 400);
     });
 
-    setTimeout(function() {
-        $('#collapse').click(function() {
+    // Botão collapse para nav em dispositivos menores
+    setTimeout(function () {
+        $('#collapse').click(function () {
             $('#MenuC').slideToggle();
             $('#MenuC').empty();
             $('#MenuC').html($('<ul></ul>').append($('nav > ul').html()));
@@ -145,26 +132,26 @@ $(document).ready(function() {
         updateNav();
     }, 500);
 
-    $(window).on('resize', function() {
+    // Responsividade nav
+    $(window).on('resize', function () {
         updateNav();
     });
 
     $('img').attr('alt', 'Não foi possível carregar a imagem, recarregue a página ou baixe a imagem para ver');
-    $('img').css({
-        'font-size': 13,
-        'text-align': 'center'
-    });
+    $('img').css({ 'font-size': 13, 'text-align': 'center' });
 
+    // Protege gifs contra menu de contexto e dnd
+    var url = location.pathname.substring(location.pathname.lastIndexOf('/') + 1).split('.')[0];
     if (url == 'gifs') {
-        $('img').on('mousedown contextmenu', function(e) {
+        $('img').on('mousedown contextmenu', function (e) {
             e.preventDefault();
         });
 
-        $('img').on('load', function() {
+        $('img').on('load', function () {
             $(this).siblings('.loader').remove();
         });
 
-        $('.backgif').click(function(e) {
+        $('.backgif').click(function (e) {
             if (!(e.target.tagName === 'IMG') && !(e.target.tagName == 'I')) return;
 
             var img = $(this).children('.gif').children('img')[0];
@@ -178,7 +165,6 @@ $(document).ready(function() {
 });
 
 function updateNav() {
-    // 860
     if ($(window).width() <= 1000) {
         $('nav > ul').hide();
         $('#collapse').show();
@@ -186,28 +172,5 @@ function updateNav() {
         $('#collapse').hide();
         $('nav > ul').show();
         $('#MenuC').hide();
-    }
-}
-
-//  MUDANDO TEXTO DE "AtivarTodos"
-
-var url = location.pathname.substring(1).split('.')[0];
-
-if (url == 'gifs') {
-    $('.ativarTodos').html('Ativar todos');
-    $('.ativarTodos').css('padding', '15');
-}
-
-if (url == 'imagens' || url == 'clipes') {
-    $('.ativarTodos').remove();
-    $('.comentar').css('top', 37);
-    $('.toggleComentar').css('top', 0);
-    $('.naoCarregando').css('top', 0);
-    $('.qntAudios').css('top', 37);
-    $('.qntGIF').css('top', 74);
-    $('.qntImg').css('top', 111);
-    if (url == 'imagens') {
-        $('.backimg').css('height', 238);
-        // $('.backimg').css('width', 270);
     }
 }
